8db4ad358f348091be49b92ee3796dc577c6954e
ChemaLx <chemav933@gmail.com>
2022-04-27 17:30:44 -0500

    nabvar

---
+++ src/app/pages/layout/navbar-comercial/navbar-comercial.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-comercial',
+  templateUrl: './navbar-comercial.component.html',
+  styleUrls: ['./navbar-comercial.component.css']
+})
+export class NavbarComercialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
