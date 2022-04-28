8f1839f640fee86c5d1aa8d073bc74f7f1809923
ChemaLx <chemav933@gmail.com>
2022-04-27 20:57:27 -0500

    perfil sintetico

--- src/app/pages/layout/perfil-sintetico/_shared/perfil-sintetico.service.ts
+++ src/app/pages/layout/perfil-sintetico/_shared/perfil-sintetico.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
+import { UtilsService } from 'src/app/shared/utils.service';
 @Injectable({
   providedIn: 'root'
 })
@@ -12,14 +14,16 @@ export class PerfilSinteticoService {
 		'Content-Type': 'application/json'
 	});
 
-  constructor(private readonly _httpClient: HttpClient) { }
+  constructor(private readonly _httpClient: HttpClient, private readonly _utilService: UtilsService) { }
 
-  probarPerfilSintetico(fechaNacimiento: string, sexo: boolean, ritmoCardiaco): Observable<HttpResponse<any>> {
-		return this._httpClient.get(`${environment.api}/usuario/procesamiento/clasificar/json/`,
+  probarPerfilSintetico(parametros: any[]): Observable<HttpResponse<any>> {
+	  console.log(this._utilService.prepararObjeto(parametros))
+		return this._httpClient.post(`${environment.api}/usuario/procesamiento/clasificar/json/`, this._utilService.prepararObjeto(parametros),
 			{
 				headers: this.httpHeaders,
 				observe: 'response',
 			}
-		)
+		).pipe(map(resp => this._utilService.handleResponse(resp), this)
+		).pipe(catchError(error => this._utilService.handleError(error)))
 	}
 }
--- src/app/pages/layout/perfil-sintetico/perfil-sintetico.component.ts
+++ src/app/pages/layout/perfil-sintetico/perfil-sintetico.component.ts
@@ -3,6 +3,7 @@ import { PerfilSinteticoService } from './_shared/perfil-sintetico.service';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import dayjs from 'dayjs';
 import es from 'dayjs/locale/es'
+import ld from 'lodash'
 
 @Component({
   selector: 'app-perfil-sintetico',
@@ -11,19 +12,78 @@ import es from 'dayjs/locale/es'
 })
 export class PerfilSinteticoComponent implements OnInit {
 
+  ultimoDia = dayjs().format('YYYY-MM-DD')
   ritmoCardiacoSlider = 60
+  formGroupPerfilSintetico: FormGroup
+  resultado = ''
 
-  constructor(private readonly _perfilSinteticoService: PerfilSinteticoService) { }
+
+  //banderas
+  isClasificacionLista = false
+  isClasificacionFailed = false
+  isCargando = false
+
+  constructor(private readonly _perfilSinteticoService: PerfilSinteticoService,
+    private readonly _formBuilder: FormBuilder,) { }
 
   ngOnInit(): void {
-  }
-  ingresarRitmoCardiaco(ritmoCardiaco: any){
-    this.ritmoCardiacoSlider = ritmoCardiaco
+    this.crearFormularioPerfilSintetico()
+    console.log(this.ultimoDia)
   }
 
+  crearFormularioPerfilSintetico() {
+    this.formGroupPerfilSintetico = this._formBuilder.group({
+      
+      
+      edad: new FormControl('', [Validators.required]),
+      ritmoCardiaco: new FormControl('', [Validators.required]),
+      inputs: new FormControl('', [Validators.required]),
+      
+    })
+    this.formGroupPerfilSintetico.controls['ritmoCardiaco'].setValue(60);
+
+  }
+
+  get f() { return this.formGroupPerfilSintetico.controls }
+
+  limpiarFormulario() {
+    this.formGroupPerfilSintetico.reset()
+  }
 
   probarPerfil(){
-    this._perfilSinteticoService.probarPerfilSintetico('22-02-1999', true, 45).subscribe(res => console.log(res))
+    this.isClasificacionLista = false
+    this.isCargando = true
+    const parametros = []
+
+    Object.keys(this.f).forEach(key => {
+      if (key === 'edad' || key === 'ritmoCardiaco') {
+        if (Boolean(this.f[key].value)) {
+          parametros.push({ parametro: ld.snakeCase(key), valor: this.f[key].value })
+        }
+      }
+      if (key === 'inputs') {
+        if (Boolean(this.f[key].value)) {
+          parametros.push({ parametro: 'sexo', valor: this.f[key].value })
+        }
+      }
+    })
+
+    console.log(parametros)
+    this._perfilSinteticoService.probarPerfilSintetico(parametros).subscribe(res => {
+      //this.limpiarFormulario()
+      this.isCargando = false
+      this.isClasificacionLista = true
+      this.resultado = res.body.clasificacionArritmia == 0 ? 'Ritmo cardiaco normal' :
+      res.body.clasificacionArritmia == 1 ? 'Taquicardia' :
+      res.body.clasificacionArritmia == 2 ? 'Bradicardia' : ''
+      
+      
+    }, err => {
+      //this.error = err.error.details
+      this.isClasificacionFailed = true
+      this.isCargando = false
+      
+    })
   }
 
   
