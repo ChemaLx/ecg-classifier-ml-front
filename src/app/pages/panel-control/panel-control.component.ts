919d3c71c907d3f334ce8b5b1b4c4dfd7c23006e
ChemaLx <chemav933@gmail.com>
2022-04-28 04:14:17 -0500

    mejora al panel de control

--- src/app/pages/panel-control/panel-control.component.ts
+++ src/app/pages/panel-control/panel-control.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import dayjs from 'dayjs';
 
 @Component({
   selector: 'app-panel-control',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PanelControlComponent implements OnInit {
 
+  horaActual = dayjs()
+  mensaje = ''
   constructor() { }
-
   ngOnInit(): void {
+    console.log(this.horaActual)
+    if(dayjs().hour(0).minute(0).second(0)['$d'] <= this.horaActual['$d'] && this.horaActual['$d'] < dayjs().hour(12).minute(0).second(0)['$d']){
+      this.mensaje = 'Buenos dias'
+    }
+    if(dayjs().hour(12).minute(0).second(0)['$d'] <= this.horaActual['$d'] && this.horaActual['$d'] < dayjs().hour(18).minute(0).second(0)['$d']){
+      this.mensaje = 'Buenas tardes'
+    }
+    if(dayjs().hour(18).minute(0).second(0)['$d'] <= this.horaActual['$d'] && this.horaActual['$d'] < dayjs().hour(23).minute(59).second(59)['$d']){
+      this.mensaje = 'Buenas noches'
+    }
   }
 
 }
