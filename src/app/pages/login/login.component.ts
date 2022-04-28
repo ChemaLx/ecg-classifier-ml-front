13395bf8c693caa849b243bec8f42664e1443d1d
ChemaLx <chemav933@gmail.com>
2022-04-27 20:57:14 -0500

    login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
+import { Router } from '@angular/router';
 import { UtilsService } from 'src/app/shared/utils.service';
+import { LoginService } from './shared/login.service';
 
 @Component({
   selector: 'app-login',
@@ -11,37 +13,45 @@ export class LoginComponent implements OnInit {
 
   //formulario
   formGroupLogin: FormGroup
+
+  //banderas 
+  isCargando = false
+  isLoginError = false
   constructor(private _formBuilder: FormBuilder,
-              private _util: UtilsService) { }
+              private _loginService: LoginService, private _router: Router) { }
 
   ngOnInit(): void {
     this.crearFormularioLogIn()
   }
   crearFormularioLogIn() {
     this.formGroupLogin = this._formBuilder.group({
-      username: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required])
+      usuario: new FormControl('', [Validators.required]),
+      contrasena: new FormControl('', [Validators.required])
     })
 
   }
 
   get f() { return this.formGroupLogin.controls }
 
-  signIn(){
+  iniciarSesion(){
+    this.isCargando = true
     let params: any[] = []
-  
+    
     Object.keys(this.f).forEach(key => {
-      if (key === 'username' || key === 'password') {
+      if (key === 'usuario' || key === 'contrasena') {
         if (Boolean(this.f[key].value)) {
           params.push({ parametro: key, valor: this.f[key].value })
         }
       }
     })
     
-    this._util.SignIn(params).subscribe(res => {
-
+    this._loginService.iniciarSesion(params).subscribe(res => {
+      console.log(res.body)
+      this.isCargando = false
+      this._router.navigate(['/panel'])
     }, err => {
-
+      this.isCargando = false
+      this.isLoginError = true
     })
   }
 
---
+++ src/app/pages/login/shared/login.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  httpHeaders = new HttpHeaders({
+		'Content-Type': 'application/json'
+	});
+
+  constructor(private _httpClient: HttpClient) { }
+
+  iniciarSesion(parametros: any[]): Observable<HttpResponse<any>> {
+    var httpParams = new HttpParams()
+    if (parametros != null && parametros != undefined && parametros.length != 0) {
+			parametros.forEach(p => {
+        console.log(p.valor)
+				httpParams = httpParams.set(p.parametro, p.valor)
+			})
+		}
+    console.log(httpParams)
+    
+		return this._httpClient.get('/iniciar-sesion',
+			{
+				headers: this.httpHeaders,
+				observe: 'response',
+        params: httpParams
+			})
+	}
+}
