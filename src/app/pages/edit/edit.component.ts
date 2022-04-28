e277dd07e0f7befbaa011b8be3c4d40a0b1c4e3d
ChemaLx <chemav933@gmail.com>
2022-04-28 02:09:54 -0500

    edit component

--- src/app/pages/edit/edit.component.ts
+++ src/app/pages/edit/edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EditService } from './shared/edit.service';
 
 @Component({
   selector: 'app-edit',
@@ -7,9 +8,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditComponent implements OnInit {
 
-  constructor() { }
+  informacionPersonal = {}
+  keys = []
+  values = []
+  informacionPersonalArray = []
+  
+
+  //banderas
+  isCargando = true
+  isEditarActive = false
+  isEditarContactoActive = false
+
+  constructor(private readonly editService: EditService) { }
 
   ngOnInit(): void {
+    this.recuperarInformacionPersonal()
+  }
+
+  recuperarInformacionPersonal() {
+    this.editService.recuperarInformacionPersonal('oaaaa-eeee-uuu-uuuu').subscribe(res => {
+      this.informacionPersonal = res.body
+      this.isCargando = false
+      /* this.keys = Object.keys(res.body)
+      this.values = Object.values(res.body)
+      let i = 0
+      console.log(this.keys)
+      this.keys.map(key => {
+        this.informacionPersonal.push({key: this.keys[i], value: this.values[i]})
+        i++
+      })
+      console.log(this.informacionPersonal) */
+    })
+  }
+  guardarNuevaInfo() {
+    this.isCargando = true
+    this.editService.recuperarInformacionPersonal('oaaaa-eeee-uuu-uuuu').subscribe(res => {
+      this.informacionPersonal = res.body
+      this.isCargando = false
+      this.isEditarActive = false
+      
+    })
+  }
+  guardarNuevaInfoContacto() {
+    this.isCargando = true
+    this.editService.recuperarInformacionPersonal('oaaaa-eeee-uuu-uuuu').subscribe(res => {
+      this.informacionPersonal = res.body
+      this.isCargando = false
+      this.isEditarContactoActive = false
+      
+    })
   }
 
 }
---
+++ src/app/pages/edit/shared/edit.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+import { UtilsService } from 'src/app/shared/utils.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EditService {
+
+  httpHeaders = new HttpHeaders({
+		'Content-Type': 'application/json'
+	});
+
+  constructor(private readonly _httpClient: HttpClient, private readonly _utilService: UtilsService) { }
+
+  recuperarInformacionPersonal(idUsuario): Observable<HttpResponse<any>> {
+	  console.log(idUsuario)
+		return this._httpClient.get(`${environment.api}/usuario/recuperar/info/`,
+			{
+				headers: this.httpHeaders,
+				observe: 'response',
+			}
+		).pipe(map(resp => this._utilService.handleResponse(resp), this)
+		).pipe(catchError(error => this._utilService.handleError(error)))
+	}
+}
