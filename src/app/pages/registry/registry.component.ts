eb2f719bf3301ef35b156db2eec9cb9afef2c4e0
ChemaLx <chemav933@gmail.com>
2022-05-20 00:33:34 -0500

    avances a rgistry

--- src/app/pages/registry/registry.component.ts
+++ src/app/pages/registry/registry.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { UtilsService } from 'src/app/shared/utils.service';
+import dayjs from 'dayjs';
 
 @Component({
   selector: 'app-registry',
@@ -9,45 +10,59 @@ import { UtilsService } from 'src/app/shared/utils.service';
 })
 export class RegistryComponent implements OnInit {
 
-    //formulario
-    formGroupSignIn: FormGroup
-    constructor(private _formBuilder: FormBuilder,
-                private _util: UtilsService) { }
-  
-    ngOnInit(): void {
-      this.crearFormularioSignIn()
-    }
-    crearFormularioSignIn() {
-      this.formGroupSignIn = this._formBuilder.group({
-        email: new FormControl('', [Validators.required, Validators.email]),
-        /* aqui agregas los demas controles (nombreo, apelidos, fecha de nacimiento o lo que quieras) */
-        password: new FormControl('', [Validators.required])
-      })
-  
-    }
-  
-    get f() { return this.formGroupSignIn.controls }
-  
-    signIn(){
-      let params: any[] = []
-    
-      Object.keys(this.f).forEach(key => {
-        if (key === 'username' || key === 'password') {
-          if (Boolean(this.f[key].value)) {
-            params.push({ parametro: key, valor: this.f[key].value })
-          }
-        }
-      })
-      console.log(params)
-      
-      /* mandas llamar tu service
-      this._util.Nombre(params).subscribe(res => {
-  
-      }, err => {
-  
-      }) */
-    }
-  
-  
+  //formulario
+  formGroupSignIn: FormGroup
+  ultimoDia = dayjs().format('YYYY-MM-DD')
+  constructor(private _formBuilder: FormBuilder,
+    private _util: UtilsService) { }
+
+  ngOnInit(): void {
+    this.crearFormularioSignIn()
   }
-  
+  crearFormularioSignIn() {
+    this.formGroupSignIn = this._formBuilder.group({
+      nombre: new FormControl('', [Validators.required]),
+      username: new FormControl('', [Validators.required]),
+      apellidoPaterno: new FormControl('', [Validators.required]),
+      apellidoMaterno: new FormControl('', [Validators.required]),
+      edad: new FormControl('', [Validators.required]),
+      sexo: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required]),
+      contrasena: new FormControl('', [Validators.required])
+    })
+
+  }
+
+  get f() { return this.formGroupSignIn.controls }
+
+  signIn() {
+    let params: any[] = []
+
+    Object.keys(this.f).forEach(key => {
+      if (key === 'nombre' || key === 'apellidoPaterno' || key === 'apellidoMaterno' || key === 'edad' || key === 'username' || key === 'contrasena' || key === 'sexo' || key === 'email') {
+        if (Boolean(this.f[key].value)) {
+          params.push({ parametro: key, valor: this.f[key].value })
+        }
+        
+      }
+      /* if (key === 'sexo') {
+        if (Boolean(this.f[key].value == 'Femenino')) {
+          params.push({ parametro: 'sexo', valor: '1' })
+        }
+        else if (Boolean(this.f[key].value == 'Masculino')) {
+          params.push({ parametro: 'sexo', valor: '0' })
+        }
+      } */
+    })
+    console.log(params)
+
+    /* mandas llamar tu service
+    this._util.Nombre(params).subscribe(res => {
+ 
+    }, err => {
+ 
+    }) */
+  }
+
+
+}
