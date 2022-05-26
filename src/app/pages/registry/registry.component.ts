e09b5506d59118ddd77200a33b5a00e4ac8a8026
ChemaLx <chemav933@gmail.com>
2022-05-25 21:24:45 -0500

    finalizacion de la pantalla de registro

--- src/app/pages/registry/registry.component.ts
+++ src/app/pages/registry/registry.component.ts
@@ -3,12 +3,14 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { UtilsService } from 'src/app/shared/utils.service';
 import dayjs from 'dayjs';
 import { RegistryService } from './shared/registry.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registry',
   templateUrl: './registry.component.html',
   styleUrls: ['./registry.component.css']
 })
+
 export class RegistryComponent implements OnInit {
 
   //formulario
@@ -18,14 +20,18 @@ export class RegistryComponent implements OnInit {
   //banderas 
   isUsuarioCreado = false 
   isErrorCreacion = false
+  isCargando = false
+  isRegistroFailed = false
+  isRegistroCheck = false
   
   constructor(private _formBuilder: FormBuilder,
               private _util: UtilsService,
-              private _registryService: RegistryService) { }
+              private _registryService: RegistryService, private _router: Router) { }
 
   ngOnInit(): void {
     this.crearFormularioSignIn()
   }
+
   crearFormularioSignIn() {
     this.formGroupSignIn = this._formBuilder.group({
       nombre: new FormControl('', [Validators.required]),
@@ -43,8 +49,9 @@ export class RegistryComponent implements OnInit {
   get f() { return this.formGroupSignIn.controls }
 
   signIn() {
+    this.isCargando = true
     let params: any[] = []
-
+    
     Object.keys(this.f).forEach(key => {
       if (key === 'nombre' || key === 'apellidoPaterno' || key === 'apellidoMaterno' || key === 'edad' || key === 'username' || key === 'contrasena' || key === 'sexo' || key === 'email') {
         if (Boolean(this.f[key].value)) {
@@ -53,13 +60,23 @@ export class RegistryComponent implements OnInit {
         
       }
     })
-
     console.log(params)
-    this._registryService.iniciarSesion(params).subscribe(res => {
-        
-    }, err => {
 
+    this._registryService.registrarUsuario(params).subscribe(res => {
+      this.isCargando = false
+      this.isRegistroCheck = true
+      setTimeout(() => {
+        this._router.navigate(['/login'])
+      }, 1200);
+    }, err => {
+      this.isCargando = false
+      this.isRegistroFailed = true
+      this.formGroupSignIn.reset()
     })
+
+
+
+
   }
 
 
--- src/app/pages/registry/shared/registry.service.ts
+++ src/app/pages/registry/shared/registry.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
+import { UtilsService } from 'src/app/shared/utils.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,23 +12,15 @@ export class RegistryService {
 		'Content-Type': 'application/json'
 	});
 
-  constructor(private _httpClient: HttpClient) { }
+  constructor(private readonly _httpClient: HttpClient, private readonly _utilService: UtilsService) { }
 
-  iniciarSesion(parametros: any[]): Observable<HttpResponse<any>> {
-    var httpParams = new HttpParams()
-    if (parametros != null && parametros != undefined && parametros.length != 0) {
-			parametros.forEach(p => {
-        console.log(p.valor)
-				httpParams = httpParams.set(p.parametro, p.valor)
-			})
-		}
-    console.log(httpParams)
-    
-		return this._httpClient.get('/iniciar-sesion',
+  registrarUsuario(parametros: any[]): Observable<HttpResponse<any>> {
+    var httpParams = new HttpParams()    
+	console.log(this._utilService.prepararObjeto(parametros))
+		return this._httpClient.post('/usuarios/usuario/guardar', this._utilService.prepararObjeto(parametros),
 			{
 				headers: this.httpHeaders,
 				observe: 'response',
-        params: httpParams
 			})
 	}
 }
