cacf9533b5ddb42463c611ad23011512c47d6d58
ChemaLx <chemav933@gmail.com>
2022-06-01 21:11:35 -0500

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  isMockEnabled: true,
+  
+	apiProcesamiento: 'http://127.0.0.1:8082',
+	apiUsuarios: 'http://127.0.0.1:8081',
+
+	isMockApiPerfilSintetico: true,
+	isMockApiIniciarSesion: true,
+	isMockApiEditarDatosInterceptor: true,
+	isMockApiHistorialInterceptor: true,
+	isMockApiNuevoEcgInterceptor: true,
+	isMockApiRegistroInterceptor: true,
 };
